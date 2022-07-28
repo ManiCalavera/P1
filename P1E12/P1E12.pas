54c1874e95f9a80da5c8dfeebf29b8f88b83dd1c
ManiCalavera <manuelgonzalezgarcia.lp@gmail.com>
2022-07-28 02:15:54 -0300

    bf

--- P1E12/P1E12.pas
+++ P1E12/P1E12.pas
@@ -14,17 +14,146 @@ Type
   End;
 
 
-  arbol = ^nodo;
+  lista = ^nodo;
   nodo = Record
     dato: registro_venta;
-    hd: arbol;
-    hi: arbol;
+    sig: lista;
   End;
 
-  vector = array [subrango] Of arbol;
 
-Procedure insertar_ordenado (Var a:arbol; r:registro_venta);
+  vector_minimos = array [subrango] Of registro_venta;
+  vector = array [subrango] Of lista;
+
+Procedure insertar_ordenado (Var l:lista; r:registro_venta);
+
+Var 
+  aux,act,ant: lista;
 Begin
-  If (a <> Nil) Then
-
+  new (aux);
+  aux^.dato := r;
+  act := l;
+  ant := act;
+  While (act <> Nil) And (act^.dato.codigo_producto < r.codigo_producto) Do
+    Begin
+      ant := act;
+      act := act^.sig;
+    End;
+  If (ant=act) Then
+    l := aux
+  Else
+    ant^.sig := aux;
+  aux^.sig := act;
 End;
+
+Procedure llenar_registro (Var r:registro_venta);
+Begin
+  writeln ('ingrese codigo');
+  readln (r.codigo_producto);
+  If (r.codigo_producto <> -1) Then
+    Begin
+      writeln ('ingrese codigo de sucursal y cantidad vendida');
+      ReadLn (r.codigo_sucursal);
+      Readln (r.cantidad_vendida);
+    End;
+End;
+
+Procedure llenar_vector (Var v:vector);
+
+Var 
+  r: registro_venta;
+Begin
+  llenar_registro (r);
+  If (r.codigo_producto <> -1) Then
+    Begin
+      insertar_ordenado (v[r.codigo_sucursal],r);
+      llenar_vector (v);
+    End;
+End;
+
+Procedure leer (l:lista; Var r:registro_venta);
+Begin
+  If (l = Nil) Then
+    r.codigo_producto := 32000
+  Else
+    Begin
+      r := l^.dato;
+      l := l^.sig;
+    End;
+End;
+
+Procedure buscar_minimo (Var v:vector; Var vm:vector_minimos; Var min:
+                         registro_venta);
+
+Var 
+  i,pos: integer;
+Begin
+  min.codigo_producto := 32000;
+  For i:=1 To cantidad_sucursales Do
+    Begin
+      If (vm[i].codigo_producto<min.codigo_producto) Then
+        Begin
+          min := vm[i];
+          pos := i;
+        End;
+    End;
+  If ( min.codigo_producto <> 32000) Then
+    Begin
+      leer (v[pos],vm[pos]);
+      v[pos] := v[pos] ^.sig;
+    End;
+End;
+
+Procedure merge (v:vector; Var l:lista);
+
+Var 
+  min,actual: registro_venta;
+  i: integer;
+  vm: vector_minimos;
+  suma: integer;
+Begin
+  l := Nil;
+  For i:=1 To cantidad_sucursales Do
+    leer (v[i],vm[i]);
+  buscar_minimo (v,vm,min);
+  While (min.codigo_producto <> 32000 ) Do
+    Begin
+      actual := min;
+      suma := 0;
+      While (min.codigo_producto= actual.codigo_producto) Do
+        Begin
+          suma := suma+min.cantidad_vendida;
+          buscar_minimo (v,vm,min);
+        End;
+      actual.cantidad_vendida := actual.cantidad_vendida + suma;
+      insertar_ordenado (l,actual);
+    End;
+End;
+
+Procedure informar_lista (l:lista);
+Begin
+  While (l <> Nil) Do
+    Begin
+      writeln;
+      writeln ('--------------------')     ;
+      writeln ('Codigo: ',l^.dato.codigo_producto);
+      writeln ('Codigo  de sucursal: ',l^.dato.codigo_sucursal);
+      writeln ('cantidad de ventas ',l^.dato.cantidad_vendida);
+      writeln;
+      l := l^.sig;
+    End;
+End;
+
+Var 
+  v: vector;
+  l: lista;
+  i: integer;
+Begin
+  For i:=1 To cantidad_sucursales Do
+    v[i] := Nil;
+  llenar_vector (v);
+  For i:=1 To cantidad_sucursales Do
+    informar_lista (v[i]);
+  merge (v,l);
+  informar_lista (l);
+
+End.
